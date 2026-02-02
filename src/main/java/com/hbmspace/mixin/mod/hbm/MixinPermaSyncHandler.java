86dd07b556d36f7a89316fdc97646053afc5a20d
TheSlize <r.region-4@yandex.ru>
2026-02-02 04:54:18 +0300

    fix: FINALLY debloated logs (fixed #9); pray that it won't break anything..

--- src/main/java/com/hbmspace/mixin/mod/hbm/MixinPermaSyncHandler.java
+++ src/main/java/com/hbmspace/mixin/mod/hbm/MixinPermaSyncHandler.java
@@ -27,8 +27,8 @@ import java.util.HashMap;
 @Mixin(value = PermaSyncHandler.class, remap = false)
 public class MixinPermaSyncHandler {
 
-    @Inject(method = "writePacket", at = @At(value = "FIELD", target = "Lcom/hbm/saveddata/satellites/SatelliteSavedData;sats:Lit/unimi/dsi/fastutil/ints/Int2ObjectOpenHashMap;", shift = At.Shift.BEFORE))
-    private static void beforeSatelliteWrite(ByteBuf buf, World world, EntityPlayerMP player, CallbackInfo ci) {
+    @Inject(method = "writePacket", at = @At("TAIL"))
+    private static void afterWritePacket(ByteBuf buf, World world, EntityPlayerMP player, CallbackInfo ci) {
         /// CBT ///
         if(world.getTotalWorldTime() % 5 == 1) { // update a little less frequently to not blast the players with large packets
             buf.writeBoolean(true);
@@ -69,10 +69,7 @@ public class MixinPermaSyncHandler {
             buf.writeBoolean(false);
         }
         /// CBT ///
-    }
 
-    @Inject(method = "writePacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayerMP;getRidingEntity()Lnet/minecraft/entity/Entity;", shift = At.Shift.BEFORE, ordinal = 0))
-    private static void beforeDesyncFixWrite(ByteBuf buf, World world, EntityPlayerMP player, CallbackInfo ci) {
         /// TIME OF DAY ///
         if(world.provider instanceof WorldProviderCelestial celestial && world.provider.getDimension() != 0) {
             buf.writeBoolean(true);
@@ -83,8 +80,8 @@ public class MixinPermaSyncHandler {
         /// TIME OF DAY ///
     }
 
-    @Inject(method = "readPacket", at = @At(value = "INVOKE", target = "Lio/netty/buffer/ByteBuf;readInt()I", ordinal = 1, shift = At.Shift.BEFORE), cancellable = true)
-    private static void beforeSatelliteRead(ByteBuf buf, World world, EntityPlayer player, CallbackInfo ci) {
+    @Inject(method = "readPacket", at = @At("TAIL"))
+    private static void afterReadPacket(ByteBuf buf, World world, EntityPlayer player, CallbackInfo ci) {
         /// CBT ///
         if(buf.readBoolean()) {
             try {
@@ -113,20 +110,13 @@ public class MixinPermaSyncHandler {
 
                 SolarSystemWorldSavedData.updateClientTraits(traitMap);
             } catch (Exception ex) {
-                // If any exception occurs, stop parsing any more bytes, they'll be unaligned
-                // We'll unset the client trait set to prevent any issues
-
                 MainRegistry.logger.catching(ex);
                 SolarSystemWorldSavedData.updateClientTraits(null);
-
-                ci.cancel();
+                return;
             }
         }
         /// CBT ///
-    }
 
-    @Inject(method = "readPacket", at = @At(value = "INVOKE", target = "Lcom/hbm/saveddata/satellites/SatelliteSavedData;setClientSats(Lit/unimi/dsi/fastutil/ints/Int2ObjectOpenHashMap;)V", shift = At.Shift.AFTER))
-    private static void afterSatelliteRead(ByteBuf buf, World world, EntityPlayer player, CallbackInfo ci) {
         /// TIME OF DAY ///
         boolean hasTimeData = buf.readBoolean();
         if(hasTimeData) {
