90104b8cc499357d5259db3212818bc6d64ff094
TheSlize <r.region-4@yandex.ru>
2026-02-28 16:05:29 +0300

    fix: crashing on cleanroom due to block replacer

--- src/main/java/com/hbmspace/handler/registires/ModBlocksReplaceHandler.java
+++ src/main/java/com/hbmspace/handler/registires/ModBlocksReplaceHandler.java
@@ -2,14 +2,11 @@ package com.hbmspace.handler.registires;
 
 import com.hbm.blocks.ModBlocks;
 import com.hbm.main.MainRegistry;
-import com.hbmspace.blocks.generic.BlockOre;
 import com.hbmspace.blocks.generic.BlockOreFluid;
+import com.hbmspace.enums.EnumAddonTypes;
 import net.minecraft.block.Block;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.IForgeRegistryEntry;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
 import static com.hbmspace.blocks.ModBlocksSpace.ore_oil_empty;
 
@@ -17,37 +14,17 @@ public class ModBlocksReplaceHandler {
 
     public static void initReplacings(RegistryEvent.Register<Block> event) {
         Block ore_oil_override = new BlockOreFluid("ore_oil", ore_oil_empty, BlockOreFluid.ReserveType.OIL).setCreativeTab(MainRegistry.blockTab).setHardness(5.0F).setResistance(10.0F);
-        Block ore_bedrock_oil_override = new BlockOreFluid("ore_bedrock_oil", null, BlockOreFluid.ReserveType.OIL).setCreativeTab(MainRegistry.blockTab).setBlockUnbreakable().setResistance(1_000_000);;
-
-        try {
-            Field regNameField = IForgeRegistryEntry.Impl.class.getDeclaredField("registryName");
-            regNameField.setAccessible(true);
-            regNameField.set(ore_oil_override, null); // it doesn't fucking comply to any logic, but it works. WORKS, mein Gott
-            regNameField.set(ore_bedrock_oil_override, null); // it doesn't fucking comply to any logic, but it works. WORKS, mein Gott
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Block ore_bedrock_oil_override = new BlockOreFluid("ore_bedrock_oil", null, BlockOreFluid.ReserveType.OIL).setCreativeTab(MainRegistry.blockTab).setBlockUnbreakable().setResistance(1_000_000);
+        // Th3_Sl1ze: lmao, EnumAddonTypes was useful not only for enums..
+        EnumAddonTypes.setInstanceField(IForgeRegistryEntry.Impl.class, "registryName", ore_oil_override, null);
+        EnumAddonTypes.setInstanceField(IForgeRegistryEntry.Impl.class, "registryName", ore_bedrock_oil_override, null);
 
         ore_oil_override.setRegistryName("hbm", "ore_oil");
         ore_bedrock_oil_override.setRegistryName("hbm", "ore_bedrock_oil");
         event.getRegistry().register(ore_oil_override);
         event.getRegistry().register(ore_bedrock_oil_override);
 
-        try {
-            Field field_oil = ModBlocks.class.getDeclaredField("ore_oil");
-            Field field_boil = ModBlocks.class.getDeclaredField("ore_bedrock_oil");
-            field_oil.setAccessible(true);
-            field_boil.setAccessible(true);
-
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
-            modifiersField.setAccessible(true);
-            modifiersField.setInt(field_oil, field_oil.getModifiers() & ~Modifier.FINAL);
-            modifiersField.setInt(field_boil, field_boil.getModifiers() & ~Modifier.FINAL);
-
-            field_oil.set(null, ore_oil_override);
-            field_boil.set(null, ore_bedrock_oil_override);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        EnumAddonTypes.setStaticField(ModBlocks.class, "ore_oil", ore_oil_override);
+        EnumAddonTypes.setStaticField(ModBlocks.class, "ore_bedrock_oil", ore_bedrock_oil_override);
     }
 }
--- src/main/java/com/hbmspace/handler/registires/ModItemsReplaceHandler.java
+++ src/main/java/com/hbmspace/handler/registires/ModItemsReplaceHandler.java
@@ -4,6 +4,7 @@ import com.hbm.blocks.ModBlocks;
 import com.hbm.items.ModItems;
 import com.hbmspace.accessors.ICanSealAccessor;
 import com.hbmspace.accessors.IHaveCorrosionProtAccessor;
+import com.hbmspace.enums.EnumAddonTypes;
 import com.hbmspace.items.ModItemsSpace;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -85,13 +86,7 @@ public class ModItemsReplaceHandler {
         ore_oil_item.setHasSubtypes(true);
         ore_oil_item.setMaxDamage(0);
 
-        try {
-            Field regNameField = IForgeRegistryEntry.Impl.class.getDeclaredField("registryName");
-            regNameField.setAccessible(true);
-            regNameField.set(ore_oil_item, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        EnumAddonTypes.setInstanceField(net.minecraftforge.registries.IForgeRegistryEntry.Impl.class, "registryName", ore_oil_item, null);
 
         ore_oil_item.setRegistryName("hbm", "ore_oil");
         event.getRegistry().register(ore_oil_item);
